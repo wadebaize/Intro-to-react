e9f225f9a92c77692074a64123874cd237a6e841
Wade Baize <wadebaize@Wades-MBP.jetpack>
2018-05-28 20:36:34 -0600

    Assignment 6 Completed

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -31,12 +31,19 @@ class App extends Component {
   this.setState({ todos: todos });
   }
 
+  deleteTodo(index){
+    const remainingTodos = this.state.todos.filter((todo, remainingTodos) => {
+      return (todo.index !==remainingTodos.index)
+    } )
+    this.setState({todos : remainingTodos })
+  }
+
   render() {
     return (
       <div className="App">
       <ul>
         { this.state.todos.map( (todo,index) =>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
         )}
       </ul>
       <form onSubmit={ (e) => this.handleSubmit(e) }>
